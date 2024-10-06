773e172d1dff002eee11c0c932933b51a0ff5b27
unknown <v.chernov5@yandex.ru>
2024-10-06 21:49:03 +0300

    решение занятия 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,9 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const rows = table.rows;
+
+  for (let i = 0; i < rows.length; i++) {
+      if (rows[i].cells[i]) {
+          rows[i].cells[i].style.backgroundColor = 'red';
+      }
+  }
 }
